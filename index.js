48e8e76f012de408171cc66e114e4f80fc14b842
Patrick McNulty <pamcnultyjr@gmail.com>
2020-02-06 14:00:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ function add(){
   return(1 + 80);
 }
 function subtract(){
-  console.log(60 - 40);
+  return(60 - 40);
 }
 
 function multiply(){
-  console.log(2 * 3.4);
+  return(2 * 3.4);
 }
 
 function divide(){
-  console.log(5.0 / 2.5);
+  return(5.0 / 2.5);
 }
