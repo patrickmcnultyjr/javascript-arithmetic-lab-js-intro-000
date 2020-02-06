700ec4cf87fe75e3988fd0cad9f97045d658ec2b
Patrick McNulty <pamcnultyjr@gmail.com>
2020-02-06 15:13:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,34 +1,5 @@
-var number = 10
-
-function add(){
-  return(1 + 80);
-}
-function subtract(){
-  return(60 - 40);
-}
-
-function multiply(){
-  return(2 * 3.4);
-}
-
-function divide(){
-  return(5.0 / 2.5);
-}
-
-
-function increment(){
-  return (number++);
-}
-
-function decrement(){
-  return (number--);
-}
-
-function makeInt(){
-  number = parseInt('5', 10);
-  return number
-}
-function preserveDecimal(){
-  number = parseFloat(75.123, 10);
-  return number;
+let a = Math.floor(Math.random() * 100)
+let b = Math.floor(Math.random() * 100)
+function add(a,b){
+  return a + b;
 }
