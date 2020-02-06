87b2683444df48fd46ce82fff0c515e10266294c
Patrick McNulty <pamcnultyjr@gmail.com>
2020-02-06 15:16:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ let b = Math.floor(Math.random() * 100)
 function add(a,b){
   return a + b;
 }
+add(a,b);
+
+let c = Math.floor(Math.random() * 100)
+let d = Math.floor(Math.random() * 100)
+function subtract(c,d){
+  return c - d;
+}
+subract(c,d)
